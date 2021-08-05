2d66be5e37b7ef073d79dd3d8fc00182c49fc323
Nipun Pal <nipun.k@noccarc.com>
2021-08-05 13:37:03 +0530

    V510_UART : First script which parses data from UART and informs about correct data.(v1) Depends on : CppLinuxSerial Library.

---
+++ Testing-Val/LinuxTestApp/testapp.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "CppLinuxSerial/SerialPort.hpp"
+
+using namespace mn::CppLinuxSerial;
+
+int main(int argc, char **argv)
+{
+	// Create serial port object and open serial port
+    SerialPort serialPort("/dev/ttyACM0", BaudRate::B_9600);
+	// SerialPort serialPort("/dev/ttyACM0", 13000);
+    serialPort.SetTimeout(-1); // Block when reading until any data is received
+	serialPort.Open();
+
+	// Write some ASCII datae
+	// serialPort0.Write("Hello");
+
+	// Read some data back
+    while(1) {
+        std::string readData;
+        serialPort.Read(readData);
+        std::cout << "Received data: " << readData << std::endl;
+    }
+
+	// Close the serial port
+	serialPort.Close();
+}

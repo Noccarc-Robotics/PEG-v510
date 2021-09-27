36c3bd943d3c6fbff7fd7b478bf541423c324b33
NikhilNoccarc21 <nikhil.p@noccarc.com>
2021-09-27 08:11:37 +0530

    Sys Change

--- FW-Dev/Power Board/Power_Board_F5525_1V2/Sys_Config.c
+++ FW-Dev/Power Board/Power_Board_F5525_1V2/Sys_Config.c
@@ -47,6 +47,7 @@ void System_Clock_Init(void)
     SetVCoreUp(2u);
     SetVCoreUp(3u);
 
+    /*
     P5SEL|=BIT2+BIT3;
 
     __bis_SR_register(SCG0);                  // Disable the FLL control loop
@@ -58,7 +59,22 @@ void System_Clock_Init(void)
     UCSCTL2 = 4u;                            // Set DCO Multiplier for 25MHz  // (N + 1) * FLLRef = Fdco // (762 + 1) * 32768 = 25.00MHz
     UCSCTL4 = SELA__DCOCLK | SELS__DCOCLK | SELM__DCOCLK;
     __bic_SR_register(SCG0);                  // Enable the FLL control loop
-    __delay_cycles(10000u);
+    __delay_cycles(10000u);*/
+
+
+
+
+    __bis_SR_register(SCG0);                  // Disable the FLL control loop
+    UCSCTL3 = SELREF__REFOCLK;    // select REFO as FLL source
+    UCSCTL6 = XT1OFF | XT2OFF;    // turn off XT1 and XT2
+    UCSCTL0 = 0x0000u;                        // Set lowest possible DCOx, MODx
+    UCSCTL1 = DCORSEL_6;                      // Set RSELx for DCO = 50 MHz
+    UCSCTL2 = 610u;                            // Set DCO Multiplier for 25MHz
+    UCSCTL4 = SELA__REFOCLK | SELS__DCOCLK | SELM__DCOCLK;
+    __bic_SR_register(SCG0);                  // Enable the FLL control loop
+    __delay_cycles(781250u);
+
+
 
     do
     {
@@ -66,6 +82,9 @@ void System_Clock_Init(void)
         SFRIFG1 &= ~OFIFG;                      // Clear fault flags
     }
     while (SFRIFG1&OFIFG);                    // Test oscillator fault flag
+
+
+
 
 }
 
